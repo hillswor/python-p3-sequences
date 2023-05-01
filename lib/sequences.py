bf19a39fae375afc18a52346d2e4431713f65123
Bruce <bruce.hillsworth@gmail.com>
2023-05-01 16:50:49 -0600

    Passed tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    a = 0
+    b = 1
+    for i in range(length):
+        sequence.append(a)
+        next_num = a + b
+        a = b
+        b = next_num
+    print(sequence)
+
+    
